50bd74ba220de51fd03f1e50f24472817a8ae043
SkayrimFan <124440955+SkayrimFan@users.noreply.github.com>
2023-02-25 13:27:22 +0300

    Add files via upload
    Дело сделано!

---
+++ js/data.js
@@ -0,0 +1,189 @@
+let albums = [
+    {
+        title: `Когда перепутал автомат с винтовкой`, //0
+        img: `solders/icon1.png`,
+        year: `1999`,
+        discription: `Почему полурослики из отдела названий придумали это?`,
+        tracks: [
+            {
+            title: `Мечта любого нормального человека`,
+            author: `00:06`,
+            time: `горшенев`,
+            src: `music/1m.mp3`
+            },            
+            {
+            title: `Какой-то непонятный фонк`,
+            author: `00:06`,
+            time: `горшенев`,
+            src: `music/2m.mp3`
+            },            
+            {
+            title: `Выговор тиммейту в катке`,
+            author: `00:06`,
+            time: `горшенев`,
+            src: `music/3m.mp3`
+            },            
+            {
+            title: `Дань памяти самому топовому аниме мира, но вообще я не очень люблю OVA версию Хеллсинга.`,
+            author: `00:06`,
+            time: `горшенев`,
+            src: `music/4m.mp3`
+            }
+        ]
+    },
+    {
+        title: `Когда разбилась линза в противогазе`, //1
+        img: `solders/icon2.png`,
+        year: `1888`,
+        discription: `Почему полурослики из отдела названий придумали это?`,
+        tracks: [
+            {
+            title: `танец злобного гения`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/1m.mp3`
+            },            {
+            title: `танец злобного гения`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/2m.mp3`
+            },            {
+                    title: `танец злобного гения`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/3m.mp3`
+            },            
+            {
+            title: `танец злобного гения`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/4m.mp3`
+            }
+        ]
+    },
+    {
+        title: `Лучшее средство от любой волезни - люгер`, //2
+        img: `solders/icon3.png`,
+        year: `1777`,
+        discription: `Почему полурослики из отдела названий придумали это?`,
+        tracks: [
+            {
+            title: `дурак и молния`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/1m.mp3`
+            },            {
+            title: `дурак и молния`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/2m.mp3`
+            },            {
+            title: `дурак и молния`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/3m.mp3`
+            },            {
+            title: `дурак и молния`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/4m.mp3`
+            }
+        ]
+    },
+    {
+        title: `Когда ты - пше тормозилка`, //3
+        img: `solders/icon4.png`,
+        year: `1666`,
+        discription: `Почему полурослики из отдела названий придумали это?`,
+        tracks: [
+            {
+            title: `смельчак и ветер`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/1m.mp3`
+            },
+            {
+            title: `смельчак и ветер`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/2m.mp3`
+            },
+            {
+            title: `смельчак и ветер`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/3m.mp3`
+            },
+            {
+            title: `смельчак и ветер`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/4m.mp3`
+            }
+        ]
+    },
+    {
+        title: `Do you wont to build a mathlab?`, //4
+        img: `solders/icon5.png`,
+        year: `1555`,
+        discription: `Почему полурослики из отдела названий придумали это?`,
+        tracks: [
+            {
+            title: `ели мясо мясники`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/1m.mp3`
+            },            
+            {
+            title: `ели мясо мясники`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/2m.mp3`
+            },            
+            {
+            title: `ели мясо мясники`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/3m.mp3`
+            },            
+            {
+            title: `ели мясо мясники`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/4m.mp3`
+            }
+        ]
+    },
+    {
+        title: `Комплектация стандартная - калаш, два глаза`, //5
+        img: `solders/icon6.png`,
+        year: `1444`,
+        discription: `Почему полурослики из отдела названий придумали это?`,
+        tracks: [
+            {
+            title: `ели мясо мужики`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/1m.mp3`
+            },            
+            {
+            title: `ели мясо мужики`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/2m.mp3`
+            },            
+            {
+            title: `ели мясо мужики`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/3m.mp3`
+            },            
+            {
+            title: `ели мясо мужики`,
+            author: `горшенев`,
+            time: `00:06`,
+            src: `music/4m.mp3`
+            }
+        ]
+    },
+]
